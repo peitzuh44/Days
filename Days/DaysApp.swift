72afd02facc16c4a08bf85a40a7e5e3cd63ad52a
Pei <peitzuh44@berkeley.edu>
2024-09-10 15:52:11 +0800

    Initial Commit

---
+++ Days/DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysApp.swift
+//  Days
+//
+//  Created by Pei-Tzu Huang on 2024/9/10.
+//
+
+import SwiftUI
+
+@main
+struct DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
